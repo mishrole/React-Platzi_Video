78a606d182210fc31b30b03fda34579e5c6b244e
Mitchell Rodríguez <mitrodle@gmail.com>
2021-01-21 13:21:44 -0500

    Add: Webpack Dev Server + npm install webpack-dev-server --save-dev

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HellowWorld from './components/HelloWorld'
 
-ReactDOM.render(<HellowWorld />, document.getElementById(app));
+ReactDOM.render(<HellowWorld />, document.getElementById('app'));
