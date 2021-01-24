5fdb5e028356c3e9ee4fafe29c74c2b1796d31fe
Mitchell Rodríguez <mitrodle@gmail.com>
2021-01-24 02:44:33 -0500

    Add: App & CarouselItem | Show Fake API Data

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -12,7 +12,11 @@ const App = () => {
 
     // useState recibe como propiedad elementos para inicializar el estado
     // no necesariamente debe ser inicializar por array
-    const [videos, setVideos] = useState([]);
+
+    // Error Cannot read property length of undefined
+    // const [videos, setVideos] = useState([]);
+
+    const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
     // useEffect recibe dos parámetros que escucha si una propiedad cambia
     // para actualizarse, si no la colocamos generamos un loop infinito
@@ -24,32 +28,34 @@ const App = () => {
         });
     }, []);
 
-    console.log(videos);
-
     return (
         <div className="App">
         <Header />
         <Search />
 
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-           </Carousel>
-        </Categories>
+        // Optional Chaining
+        {videos.mylist?.length > 0 &&
+            <Categories title="Mi lista">
+                <Carousel>
+                    <CarouselItem />
+                </Carousel>
+            </Categories> 
+        }
 
         <Categories title="Tendencias">
             <Carousel>
-                <CarouselItem />
-                <CarouselItem />
+                {
+                    videos.trends.map(item => <CarouselItem key={item.id} {...item} />)
+                }
+
            </Carousel>
         </Categories>
 
         <Categories title="Originales de Platzi Video">
             <Carousel>
-                <CarouselItem />
+                {
+                    videos.originals.map(item => <CarouselItem key={item.id} {...item} />)
+                }
            </Carousel>
         </Categories>
 
