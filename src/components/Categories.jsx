f050ef90a64b7b807f185cff1cc18b1e72b5ee2d
Mitchell Rodríguez <mitrodle@gmail.com>
2021-01-23 22:11:02 -0500

    Fix: Categories & Footer | Class -> ClassName

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ import '../assets/styles/components/Categories.scss';
 
 const Categories = ({ children }) => (
     <div className="categories">
-        <h3 class="categories__title">Mi lista</h3>
+        <h3 className="categories__title">Mi lista</h3>
         {children}
     </div>
 );
